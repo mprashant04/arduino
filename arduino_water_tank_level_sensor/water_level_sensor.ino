a215f4dd528042f2ce336d5e51d46f16dc66594a
pm <pman.social04@gmail.com>
2021-08-03 07:11:55 -0500

    dddd

--- arduino_water_tank_level_sensor/water_level_sensor.ino
+++ arduino_water_tank_level_sensor/water_level_sensor.ino
@@ -1,5 +1,5 @@
 //Calibrate these values as per pot signal value at min & max water level, notes maintained in google drive doc. Keep 5-8 points margin above acrual full/empty pot readings
-#define   WATER_SIGNAL_MIN                            255
+#define   WATER_SIGNAL_MIN                            260
 #define   WATER_SIGNAL_MAX                            720
 
 #define   WATER_ALERT_LEVEL_H1                        80      //main tank about to get full
