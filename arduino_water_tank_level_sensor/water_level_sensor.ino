014ff71382b6435a315a93659f5d2a02b944f789
pm <pman.social04@gmail.com>
2021-08-09 02:26:55 -0500

    dd

--- arduino_water_tank_level_sensor/water_level_sensor.ino
+++ arduino_water_tank_level_sensor/water_level_sensor.ino
@@ -210,10 +210,9 @@ void playWaterTankFillingStartedAlert(){
         if (waterTankFillingInProgress) 
           playTone(TONE_REPEAT, 10, 300,150,300,150,80,80,80,9000, TONE_ARG_EOL);
         else
-          playTone(TONE_REPEAT, 3, 80,80,250,500,80,80,250,9000, TONE_ARG_EOL);
+          playTone(TONE_REPEAT, 2, 80,80,250,500,80,80,250,5000, TONE_ARG_EOL);
     }
 }
-
 
 
 boolean isWaterReadingUpdated(boolean resetStatusIfChanged){
